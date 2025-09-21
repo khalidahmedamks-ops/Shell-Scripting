be804482a2bbd57c5801ae2c79042e316118e921
sk khalid <khalidaws2024@gmail.com>
2025-09-21 22:42:11 +0530

    Shell-scripting

---
+++ 03-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Khalid
+PERSON2=Lakshman
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: How are you $PERSON1"
+echo "$PERSON1: what are you learning"
+echo "$PERSON2: Iam learning Shell scripting basic concepts"
