13bdfc2224c4ad794ff273cf516bc5a7b4b4a798
sk khalid <khalidaws2024@gmail.com>
2025-09-21 22:28:34 +0530

    Shell-scripting

---
+++ 02-Conversations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Khalid:: How are you lakshman"
+echo "Lakshmn:: How are you khalid"
+echo "Khalid:: what are you learning"
+echo "Lakshmna:: Iam learning Shell scripting basic concepts"
